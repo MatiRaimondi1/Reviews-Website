01896df20b502e5d4aa04163f27a32e8f313ba40
Matias Raimondi <matiraimondi1@gmail.com>
2025-05-02 12:39:21 -0300

    Movies module finished.

---
+++ src/peliculas/peliculas.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { PeliculasService } from './services/peliculas.service';
+import { PeliculasController } from './controllers/peliculas.controller';
+import { Pelicula } from './entities/pelicula.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Pelicula])
+  ],
+  providers: [PeliculasService],
+  controllers: [PeliculasController]
+})
+export class PeliculasModule {}
