c76b6c3a110f9aa59b90fb3608aade91d4c802f8
Matias Raimondi <matiraimondi1@gmail.com>
2025-05-02 13:17:28 -0300

    Updated database connection data to support dotenv.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,20 @@ import { PeliculasModule } from './peliculas/peliculas.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: process.env.USERNAME,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       retryDelay: 3000,
