65cb9a960c215b86ce558fb44f965093c22c1b32
Matias Raimondi <matiraimondi1@gmail.com>
2025-05-18 18:24:09 -0300

    Group module finished.

---
+++ src/grupos/grupo.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Grupo } from "./entities/grupo.entity";
+import { MembresiaGrupo } from "./entities/membresiaGrupo.entity";
+import { User } from "src/users/entities/user.entity";
+import { GrupoController } from "./controllers/grupo.controller";
+import { GrupoService } from "./services/grupo.service";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Grupo, MembresiaGrupo, User])],
+    controllers: [GrupoController],
+    providers: [GrupoService],
+})
+export class GrupoModule {}
