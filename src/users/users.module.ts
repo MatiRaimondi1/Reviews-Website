fd0c1fbb8b70d2f1714bafd9557c7e819449d6d7
Santino <agatiello.santino@gmail.com>
2025-05-19 16:18:07 -0300

    Adds documentation for Users Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,6 +4,10 @@ import { UsersController } from './controllers/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
+/**
+ * Encapsula los providers de la entidad Users, y define que partes 
+ * pueden importarse en otros modulos
+ */
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
