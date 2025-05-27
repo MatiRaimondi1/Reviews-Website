731165265eadb197c27bad74851216f1f0a5c9fc
Santino <agatiello.santino@gmail.com>
2025-05-27 15:34:24 -0300

    Adds documentation to Reunion Service

--- src/reuniones/services/reunion.service.ts
+++ src/reuniones/services/reunion.service.ts
@@ -6,14 +6,30 @@ import { Repository } from "typeorm";
 import { User } from "src/users/entities/user.entity";
 import { Grupo } from "src/grupos/entities/grupo.entity";
 
+/**
+ * Servicio encargado de gestionar las operaciones relacionadas con las reuniones
+ */
 @Injectable()
 export class ReunionService {
+
+    /**
+     * Inyecta los repositorios relativos a las reuniones
+     * @param reunionRepo Repositorio de reuniones
+     * @param userRepo Repositorio de usuarios
+     * @param grupoRepo Repositorio de grupos
+     */
     constructor(
         @InjectRepository(Reunion) private reunionRepo: Repository<Reunion>,
         @InjectRepository(User) private userRepo: Repository<User>,
         @InjectRepository(Grupo) private grupoRepo: Repository<Grupo>,
     ) {}
 
+    /**
+     * Crea una reunion
+     * @param userId ID del usuario que crea la reunion
+     * @param dto el DTO establecido para la creacion de reuniones
+     * @returns Promesa con la creacion de la reunion
+     */
     async create(userId: number, dto: CreateReunionDto) {
         const user = await this.findUserWithGroups(userId);
         
@@ -39,6 +55,12 @@ export class ReunionService {
         return this.reunionRepo.save(reunion);
     }
 
+    /**
+     * Borra una reunion
+     * @param userId ID del usuario que borra la reunion
+     * @param reunionId ID de la reunion a borrar
+     * @returns Mensaje de confirmacion de la eliminacion de la reunion
+     */
     async delete(userId: number, reunionId: number) {
         const user = await this.findUserWithGroups(userId);
 
@@ -61,6 +83,12 @@ export class ReunionService {
         return { message: 'Reunión eliminada correctamente.' };
     }
 
+    /**
+     * Obtiene la reunion vigente de un grupo
+     * @param userId Usuario que hace el pedido
+     * @param grupoId ID del grupo cuya reunion se quiere obtener
+     * @returns Promesa de la reunion obtenida
+     */
     async getReunionByGrupo(userId: number, grupoId: number) {
         const user = await this.findUserWithGroups(userId);
 
@@ -82,6 +110,11 @@ export class ReunionService {
         return reunion;
     }
 
+    /**
+     * Busca si un usuario pertenece a un grupo en particular
+     * @param userId ID del usuario a buscar
+     * @returns El usuario, en caso de que se haya obtenido
+     */
     private async findUserWithGroups(userId: number) {
         const user = await this.userRepo.findOne({
             where: { id: userId },
