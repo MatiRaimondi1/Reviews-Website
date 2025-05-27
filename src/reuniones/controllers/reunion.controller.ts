9bf0c558a49b94d8f5a1f23cb4711471729c4764
Santino <agatiello.santino@gmail.com>
2025-05-27 15:34:16 -0300

    Adds documentation to Reunion Controller

--- src/reuniones/controllers/reunion.controller.ts
+++ src/reuniones/controllers/reunion.controller.ts
@@ -3,23 +3,49 @@ import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 import { ReunionService } from "../services/reunion.service";
 import { CreateReunionDto } from "../dto/create-reunion.dto";
 
+/**
+ * Controlador encargado de manejar las requests relativas a las reuniones
+ */
+
 @Controller('api/reuniones')
 @UseGuards(JwtAuthGuard)
 export class ReunionController {
+    /**
+     * Inyecta el servicio de reuniones
+     * @param reunionService Servicio con la logica de negocio de Reuniones
+     */
     constructor(private readonly reunionService: ReunionService) {}
 
+    /**
+     * Crea una reunion
+     * @param dto el DTO definido para la creacion de una reunion
+     * @param req el objeto de la request de HTML
+     * @returns La reunion creada
+     */
     @Post()
     async createReunion(@Body() dto: CreateReunionDto, @Request() req) {
         const userId = req.user['id'];
         return this.reunionService.create(userId, dto);
     }
 
+    /**
+     * Borra una reunion
+     * @param id ID de la reunion a borrar
+     * @param req el objeto de la request de HTML
+     * @returns Mensaje con la confirmacion de la eliminacion de la reunion
+     */
     @Delete(':id')
     async deleteReunion(@Param('id', ParseIntPipe) id: number, @Request() req) {
         const userId = req.user['id'];
         return this.reunionService.delete(userId, id)
     }
 
+    /**
+     * Obtiene una reunion en base a su ID
+     * @param id ID de la reunion a buscar
+     * @param req el objeto de la request de HTML
+     * @returns La reunion encontrada, si se encuentra
+     */
     @Get(':id')
     async getReunion(@Param('id', ParseIntPipe) id: number, @Request() req) {
         const userId = req.user['id'];
